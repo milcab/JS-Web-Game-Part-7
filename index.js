6f9e32dd7e89dc2e3ffae03f8c4e3907c2ebf99d
milcab <milcarodriguez23@gmail.com>
2021-11-08 14:33:58 -0500

    web-game-7

--- index.js
+++ index.js
@@ -5,7 +5,37 @@ const pc = newPlayableCharacter(100, 110)
 const npc = newNonPlayableCharacter(50, 300)
 
 // have the NPC start walking east immediately
-npc.walkEast()
+async function moveNPC() {
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
+}
+
+async function runAgain() {
+    let counter = 0
+    while (counter < 10) {
+        counter++
+        console.log(counter)
+        await moveNPC()
+    } 
+
+    
+}
+
+runAgain()
+
+
+// North for 1400 ms
+// East for 1200 ms
+// South for 300 ms
+// East for 1500 ms
+// South for 1500 ms
+// West for 2700 ms
+// North for 400 ms
 
 // Create the inventory
 const inventory = newInventory()
